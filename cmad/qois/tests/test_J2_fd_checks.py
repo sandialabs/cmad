62b08910ba8cc3bd4c1914bc7bae500f03971cc3
Tom Seidl <dtseidl@sandia.gov>
2024-04-23 15:06:10 -0600

    working on FD check for hessian

--- cmad/qois/tests/test_J2_fd_checks.py
+++ cmad/qois/tests/test_J2_fd_checks.py
@@ -11,17 +11,25 @@ from cmad.solver.nonlinear_solver import newton_solve
 from cmad.test_support.test_problems import J2AnalyticalProblem
 
 
+def evaluate_hessian(qoi, hs=np.logspace(-2, -10, 9)):
+    model = qoi.model()
+    flat_active_values = model.parameters.flat_active_values(True)
+    num_active_params = model.parameters.num_active_params
+    hessian_obj = Objective(qoi, "direct-adjoint hessian")
+    return hessian_obj.evaluate(flat_active_values)
+
+
 def fd_grad_check_components(qoi, hs=np.logspace(-2, -10, 9)):
 
     model = qoi.model()
     flat_active_values = model.parameters.flat_active_values(True)
     num_active_params = model.parameters.num_active_params
 
-    forward_obj = Objective(qoi, gradient_type="forward_sens")
-    adjoint_obj = Objective(qoi, gradient_type="adjoint")
+    direct_obj = Objective(qoi, sensitivity_type="direct gradient")
+    adjoint_obj = Objective(qoi, sensitivity_type="adjoint gradient")
 
-    J_ref, fs_grad_ref = \
-        forward_obj.evaluate(flat_active_values)
+    J_ref, direct_grad_ref = \
+        direct_obj.evaluate(flat_active_values)
     J_ref, adjoint_grad_ref = \
         adjoint_obj.evaluate(flat_active_values)
 
@@ -41,7 +49,7 @@ def fd_grad_check_components(qoi, hs=np.logspace(-2, -10, 9)):
             J_minus = compute_fun(qoi)
 
             fs_fd_error[kk, ii] = np.abs((J_plus - J_minus) / (2. * h)
-                                         - fs_grad_ref[kk])
+                                         - direct_grad_ref[kk])
 
             adjoint_fd_error[kk, ii] = np.abs((J_plus - J_minus) / (2. * h)
                                               - adjoint_grad_ref[kk])
@@ -55,18 +63,18 @@ def fd_grad_check(qoi, hs=np.logspace(-2, -10, 9), seed=22):
     flat_active_values = model.parameters.flat_active_values(True)
     num_active_params = model.parameters.num_active_params
 
-    forward_obj = Objective(qoi, gradient_type="forward_sens")
-    adjoint_obj = Objective(qoi, gradient_type="adjoint")
+    direct_obj = Objective(qoi, sensitivity_type="direct gradient")
+    adjoint_obj = Objective(qoi, sensitivity_type="adjoint gradient")
 
-    J_ref, fs_grad_ref = \
-        forward_obj.evaluate(flat_active_values)
+    J_ref, direct_grad_ref = \
+        direct_obj.evaluate(flat_active_values)
     J_ref, adjoint_grad_ref = \
         adjoint_obj.evaluate(flat_active_values)
 
     np.random.seed(seed)
     d = np.random.uniform(low=-1.0, size=num_active_params)
 
-    fs_dir_deriv_ref = d.dot(fs_grad_ref)
+    fs_dir_deriv_ref = d.dot(direct_grad_ref)
     adjoint_dir_deriv_ref = d.dot(adjoint_grad_ref)
 
     fs_fd_error = np.zeros(len(hs))
@@ -118,68 +126,80 @@ def compute_fun(qoi):
     return J
 
 
-class TestJ2FDChecks(unittest.TestCase):
-    def test_J2_finite_difference_grads(self):
+#class TestJ2FDChecks(unittest.TestCase):
+#def test_J2_finite_difference_grads(self):
+def test_J2_finite_difference_grads():
 
-        J2_analytical_problem = J2AnalyticalProblem()
+    # with parameter scaling
+    #J2_analytical_problem = J2AnalyticalProblem()
 
-        # numbers of steps to run solver for (does not include the IC)
-        num_steps = 100
+    # without parameter scaling
+    scale_params = True
+    J2_analytical_problem = J2AnalyticalProblem(scale_params)
 
-        # uniaxial stress analytical solution
-        stress_mask = np.zeros((3, 3))
-        stress_mask[0, 0] = 1.
-        max_alpha = 0.5
-        stress, strain, alpha = \
-            J2_analytical_problem.analytical_solution(stress_mask, max_alpha,
-                                                      num_steps)
+    # numbers of steps to run solver for (does not include the IC)
+    num_steps = 100
 
-        # construct the global state variable F
-        def_type = DefType.FULL_3D
-        ndims = def_type_ndims(def_type)
-        I = np.eye(ndims)
-        F = np.repeat(I[:, :, np.newaxis], num_steps + 1, axis=2)
-        F[0, 0, 1:] += strain[0, 0, :]
-        F[1, 1, 1:] += strain[1, 1, :]
-        F[2, 2, 1:] += strain[2, 2, :]
+    # uniaxial stress analytical solution
+    stress_mask = np.zeros((3, 3))
+    stress_mask[0, 0] = 1.
+    max_alpha = 0.5
+    stress, strain, alpha = \
+        J2_analytical_problem.analytical_solution(stress_mask, max_alpha,
+                                                  num_steps)
 
-        # weight for calibration qoi
-        weight = np.zeros((3, 3))
-        weight[0, 0] = 1.
+    # construct the global state variable F
+    def_type = DefType.FULL_3D
+    ndims = def_type_ndims(def_type)
+    I = np.eye(ndims)
+    F = np.repeat(I[:, :, np.newaxis], num_steps + 1, axis=2)
+    F[0, 0, 1:] += strain[0, 0, :]
+    F[1, 1, 1:] += strain[1, 1, :]
+    F[2, 2, 1:] += strain[2, 2, :]
 
-        model = SmallRateElasticPlastic(J2_analytical_problem.J2_parameters,
-                                        def_type)
+    # weight for calibration qoi
+    weight = np.zeros((3, 3))
+    weight[0, 0] = 1.
 
-        # tweak the parameters to make the solution not uniaxial
-        new_params = np.array([210., 21., 109.])
+    model = SmallRateElasticPlastic(J2_analytical_problem.J2_parameters,
+                                    def_type)
 
-        zero_data = np.zeros((3, 3, num_steps + 1))
-        qoi = Calibration(model, F, zero_data, weight)
+    # tweak the parameters to make the solution not uniaxial
+    new_params = np.array([210., 21., 109.])
 
-        model.parameters.set_active_values_from_flat(new_params, False)
-        fs_fd_dir_deriv_error, adjoint_fd_dir_deriv_error = \
-            fd_grad_check(qoi, seed=10)
+    zero_data = np.zeros((3, 3, num_steps + 1))
+    qoi = Calibration(model, F, zero_data, weight)
 
-        model.parameters.set_active_values_from_flat(new_params, False)
-        fs_fd_component_error, adjoint_fd_component_error = \
-            fd_grad_check_components(qoi)
+    model.parameters.set_active_values_from_flat(new_params, False)
+    fs_fd_dir_deriv_error, adjoint_fd_dir_deriv_error = \
+        fd_grad_check(qoi, seed=10)
 
-        assert np.allclose(fs_fd_dir_deriv_error,
-                           adjoint_fd_dir_deriv_error)
+    model.parameters.set_active_values_from_flat(new_params, False)
+    fs_fd_component_error, adjoint_fd_component_error = \
+        fd_grad_check_components(qoi)
 
-        error_drop_tol = 4.
-        min_fd_error = np.min(fs_fd_dir_deriv_error)
-        max_fd_error = np.max(fs_fd_dir_deriv_error)
-        log10_error_drop = np.log10(max_fd_error / min_fd_error)
-        assert log10_error_drop > error_drop_tol
+    assert np.allclose(fs_fd_dir_deriv_error,
+                       adjoint_fd_dir_deriv_error)
 
-        diff_tol = 1e-6
-        fd_components_diff = fs_fd_component_error \
-            - adjoint_fd_component_error
-        assert np.linalg.norm(fd_components_diff) < diff_tol
+    error_drop_tol = 4.
+    min_fd_error = np.min(fs_fd_dir_deriv_error)
+    max_fd_error = np.max(fs_fd_dir_deriv_error)
+    log10_error_drop = np.log10(max_fd_error / min_fd_error)
+    assert log10_error_drop > error_drop_tol
+
+    diff_tol = 1e-6
+    fd_components_diff = fs_fd_component_error \
+        - adjoint_fd_component_error
+    assert np.linalg.norm(fd_components_diff) < diff_tol
+
+    hessian = evaluate_hessian(qoi)
+
+    assert False
 
 
-if __name__ == "__main__":
-    J2_FD_checks_test_suite = unittest.TestLoader().loadTestsFromTestCase(
-        TestJ2FDChecks)
-    unittest.TextTestRunner(verbosity=2).run(J2_FD_checks_test_suite)
+#if __name__ == "__main__":
+#    J2_FD_checks_test_suite = unittest.TestLoader().loadTestsFromTestCase(
+#        TestJ2FDChecks)
+#    unittest.TextTestRunner(verbosity=2).run(J2_FD_checks_test_suite)
+
+test_J2_finite_difference_grads()
