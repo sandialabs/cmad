8d9a62509f060c74fa42865447cd6fb8bf7996e7
Tom Seidl <dtseidl@sandia.gov>
2024-06-18 11:43:21 -0600

    hill multi-test calibration
    review in progress

--- cmad/calibrations/al7079/hill_calibration_rot.py
+++ cmad/calibrations/al7079/hill_calibration_rot.py
@@ -23,18 +23,11 @@ from cmad.objectives.objective import Objective
 def compute_cauchy(model, F, Rmat):
 
     model.parameters.values["rotation matrix"] = Rmat
-    num_steps = 50
-    ndims = def_type_ndims(def_type)
-    I = np.eye(ndims)
-    F = np.repeat(I[:, :, np.newaxis], num_steps + 1, axis=2)
-    epsilon_11 = np.linspace(0, 5. * 0.02, num_steps + 1)
-    F[0, 0, :] += epsilon_11
 
     xi_at_step = [[None, None, None] for ii in range(num_steps + 1)]
     model.set_xi_to_init_vals()
     model.store_xi(xi_at_step, model.xi_prev(), 0)
     cauchy = np.zeros((3, 3, num_steps + 1))
-
 
     for step in range(1, num_steps + 1):
         u = [F[:, :, step]]
@@ -49,13 +42,12 @@ def compute_cauchy(model, F, Rmat):
         cauchy[:, :, step] = model.Sigma().copy()
 
         model.advance_xi()
-        
 
     return jnp.stack([
-            cauchy[1,1],
-            np.array([xi_at_step[ii][2][0]-1. for ii in range(num_steps+1)]),
-            np.array([xi_at_step[ii][2][1]-1. for ii in range(num_steps+1)]),
-         ],axis=0)
+        cauchy[1, 1, :],
+        np.array([xi_at_step[ii][2][0] - 1. for ii in range(num_steps + 1)]),
+        np.array([xi_at_step[ii][2][1] - 1. for ii in range(num_steps + 1)])],
+        axis=0)
 
 
 alpha_angles, alpha_sigma_c_values, alpha_ratio_c_values, R_alphas = \
@@ -75,34 +67,30 @@ p_voce = np.array([1., 200.]) # need to fiddle with these
 
 def_type = DefType.UNIAXIAL_STRESS
 params_true = params_hill_voce(p_elastic, p_hill, p_voce)
-
-
-
-
 model_true = SmallElasticPlastic(params_true, def_type, uniaxial_stress_idx=1)
-Rmats = R_alphas + R_betas + R_gammas
+
 num_steps = 50
 ndims = def_type_ndims(def_type)
 I = np.eye(ndims)
 F = np.repeat(I[:, :, np.newaxis], num_steps + 1, axis=2)
 epsilon_11 = np.linspace(0, 5. * 0.02, num_steps + 1)
 F[0, 0, :] += epsilon_11
-data = np.stack([compute_cauchy(model_true,F,Rmat) for Rmat in Rmats],axis=0)
 
+Rmats = R_alphas + R_betas + R_gammas
+data = np.stack([compute_cauchy(model_true, F, Rmat) for Rmat in Rmats], axis=0)
 
-
-p_hill_learned = np.r_[np.array(Y), np.ones(6)]
-params = params_hill_voce(p_elastic, p_hill_learned, p_voce)
+p_hill_J2_equivalent = np.r_[np.array(Y), np.ones(6)]
+params = params_hill_voce(p_elastic, p_hill_J2_equivalent, p_voce)
 model = SmallElasticPlastic(params, def_type, uniaxial_stress_idx=1)
 initial_guess = model.parameters.flat_active_values(True).copy()
 num_active_params = model.parameters.num_active_params
 opt_bounds = params.opt_bounds
 
-weights_qoi = [2e-3,2e1,2e1]
-weights_time = np.ones(num_steps+1)
+weights_qoi = [2e-3, 2e1, 2e1]
+weights_time = np.ones(num_steps + 1)
 weights_time[0:10] = 0.
 qoi = UniaxialCalibration(model, F, data[0], weights_qoi)
-objective = Objective(qoi, sensitivity_type="adjoint gradient", weights = weights_time)
+objective = Objective(qoi, sensitivity_type="adjoint gradient", weights=weights_time)
 
 def multiobjective(Rmats, data):
     def mobjective(varlist):
@@ -116,9 +104,13 @@ def multiobjective(Rmats, data):
 
             J_total += J
             grad_total += grad 
-        return J_total,grad_total
+        return J_total, grad_total
     return mobjective
 
 opt_params, fun_vals, cvg_dict = fmin_l_bfgs_b(
     multiobjective(Rmats,data), params_true.flat_active_values(True)+.1, 
     bounds=opt_bounds, iprint=1, maxiter=400)
+
+model.parameters.set_active_values_from_flat(opt_params)
+unscaled_opt_params = model.parameters.flat_active_values()
+param_diff = np.linalg.norm(unscaled_opt_params - fit_params)
