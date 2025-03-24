d961107bf17fa4b413d6cac10bc7c0c1cc70af7f
Tom Seidl <dtseidl@sandia.gov>
2025-03-24 12:49:32 -0600

    [al7079] fix for jax effective stress normals
    The jax Hill and Barlat effective stress functions were giving
    incorrect normals via jax.grad(...).

--- cmad/verification/functions.py
+++ cmad/verification/functions.py
@@ -23,6 +23,7 @@ def J2_yield_normal(cauchy):
     return normal
 
 
+# normal of this is not jax safe due to symmetry assumption
 def hill_yield(cauchy, hill_params):
     F, G, H, L, M, N = hill_params
 
@@ -36,6 +37,7 @@ def hill_yield(cauchy, hill_params):
     return phi
 
 
+# analytic normal
 def hill_yield_normal(cauchy, hill_params):
     F, G, H, L, M, N = hill_params
 
@@ -53,35 +55,21 @@ def hill_yield_normal(cauchy, hill_params):
     return normal
 
 
+# written in terms of full cauchy to be jax safe
 def jax_hill_yield(cauchy, hill_params):
     F, G, H, L, M, N = hill_params
 
     phi = jnp.sqrt(F * (cauchy[1, 1] - cauchy[2, 2])**2
                    + G * (cauchy[2, 2] - cauchy[0, 0])**2
                    + H * (cauchy[0, 0] - cauchy[1, 1])**2
-                   + 2. * (L * cauchy[1, 2]**2
-                           + M * cauchy[0, 2]**2
-                           + N * cauchy[0, 1]**2))
+                   + L * cauchy[1, 2]**2
+                   + M * cauchy[0, 2]**2
+                   + N * cauchy[0, 1]**2
+                   + L * cauchy[2, 1]**2
+                   + M * cauchy[2, 0]**2
+                   + N * cauchy[1, 0]**2)
 
     return phi
-
-
-def jax_hill_yield_normal(cauchy, hill_params):
-    F, G, H, L, M, N = hill_params
-
-    n_00 = (G + H) * cauchy[0, 0] - H * cauchy[1, 1] - G * cauchy[2, 2]
-    n_11 = (F + H) * cauchy[1, 1] - H * cauchy[0, 0] - F * cauchy[2, 2]
-    n_22 = (G + F) * cauchy[2, 2] - G * cauchy[0, 0] - F * cauchy[1, 1]
-    n_01 = N * cauchy[0, 1]
-    n_02 = M * cauchy[0, 2]
-    n_12 = L * cauchy[1, 2]
-
-    normal = jnp.array([[n_00, n_01, n_02],
-                       [n_01, n_11, n_12],
-                       [n_02, n_12, n_22]]) \
-        / jax_hill_yield(cauchy, hill_params)
-
-    return normal
 
 
 def jax_unpack_barlat_params(barlat_params):
@@ -98,16 +86,18 @@ def jax_unpack_barlat_params(barlat_params):
         [-2. * sp_21 + sp_23, sp_21 + sp_23, sp_21 - 2. * sp_23],
         [-2. * sp_31 + sp_32, sp_31 - 2. * sp_32, sp_31 + sp_32]]) / 3.
     L_sp_lower_right = jnp.diag(jnp.array([sp_44, sp_55, sp_66]))
-    L_sp = jnp.vstack((jnp.c_[L_sp_upper_left, jnp.zeros((3, 3))],
-                       jnp.c_[jnp.zeros((3, 3)), L_sp_lower_right]))
+    L_sp = jnp.vstack((jnp.c_[L_sp_upper_left, jnp.zeros((3, 3)), jnp.zeros((3, 3))],
+                       jnp.c_[jnp.zeros((3, 3)), L_sp_lower_right, jnp.zeros((3, 3))],
+                       jnp.c_[jnp.zeros((3, 3)), jnp.zeros((3, 3)), L_sp_lower_right]))
 
     L_dp_upper_left = jnp.array([
         [dp_12 + dp_13, -2. * dp_12 + dp_13, dp_12 - 2. * dp_13],
         [-2. * dp_21 + dp_23, dp_21 + dp_23, dp_21 - 2. * dp_23],
         [-2. * dp_31 + dp_32, dp_31 - 2. * dp_32, dp_31 + dp_32]]) / 3.
     L_dp_lower_right = jnp.diag(jnp.array([dp_44, dp_55, dp_66]))
-    L_dp = jnp.vstack((jnp.c_[L_dp_upper_left, jnp.zeros((3, 3))],
-                       jnp.c_[jnp.zeros((3, 3)), L_dp_lower_right]))
+    L_dp = jnp.vstack((jnp.c_[L_dp_upper_left, jnp.zeros((3, 3)), jnp.zeros((3, 3))],
+                       jnp.c_[jnp.zeros((3, 3)), L_dp_lower_right, jnp.zeros((3, 3))],
+                       jnp.c_[jnp.zeros((3, 3)), jnp.zeros((3, 3)), L_dp_lower_right]))
 
     return L_sp, L_dp
 
@@ -118,7 +108,10 @@ def jax_flatten_stress(stress_matrix):
                              stress_matrix[2, 2],
                              stress_matrix[0, 1],
                              stress_matrix[1, 2],
-                             stress_matrix[2, 0]])
+                             stress_matrix[2, 0],
+                             stress_matrix[1, 0],
+                             stress_matrix[2, 1],
+                             stress_matrix[0, 2]])
 
     return flat_stress
 
@@ -126,12 +119,12 @@ def jax_flatten_stress(stress_matrix):
 def jax_unflatten_stress(flat_stress):
     stress_matrix = jnp.array([[flat_stress[0],
                                 flat_stress[3],
-                                flat_stress[5]],
-                               [flat_stress[3],
+                                flat_stress[8]],
+                               [flat_stress[6],
                                 flat_stress[1],
                                 flat_stress[4]],
                                [flat_stress[5],
-                                flat_stress[4],
+                                flat_stress[7],
                                 flat_stress[2]]])
 
     return stress_matrix
